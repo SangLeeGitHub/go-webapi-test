00eb4c69082685f0febb188458ce35a75d08faf9
unknown <Jkim9546@conestogac.on.ca>
2020-04-13 17:49:49 -0400

    Add a function for Jerry

--- webapi.go
+++ webapi.go
@@ -1,16 +1,19 @@
 package main
- 
+
 import (
-    "net/http"
+	"net/http"
 )
- 
+
 func main() {
-    http.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
-        w.Write([]byte("Hello World"))
-    })
-    http.HandleFunc("/sang", func(w http.ResponseWriter, req *http.Request) {
-        w.Write([]byte("Hello sang"))
-    })
- 
-    http.ListenAndServe(":5000", nil)
+	http.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+	http.HandleFunc("/sang", func(w http.ResponseWriter, req *http.Request) {
+		w.Write([]byte("Hello sang"))
+	})
+	http.HandleFunc("/Jerry", func(w http.ResponseWriter, req *http.Request) {
+		w.Write([]byte("Hello Jerry"))
+	})
+
+	http.ListenAndServe(":5000", nil)
 }
