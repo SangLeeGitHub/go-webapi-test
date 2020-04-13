bad806aca24b89bb727beabd90afb2b6e5c2997e
Jung Hoon <sualokin2012@gmail.com>
2020-04-13 00:06:44 -0400

    Add a function for Jung

--- webapi.go
+++ webapi.go
@@ -11,6 +11,9 @@ func main() {
     http.HandleFunc("/sang", func(w http.ResponseWriter, req *http.Request) {
         w.Write([]byte("Hello sang"))
     })
+    http.HandleFunc("/jung", func(w http.ResponseWriter, req *http.Request) {
+        w.Write([]byte("Hello jung"))
+    })
  
     http.ListenAndServe(":5000", nil)
 }
